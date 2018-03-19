2633622dc076f6def778619014beaff7287736ad
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-19 16:43:46 +0800

    Initial Commit

---
+++ IdentifyThis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdentifyThis
+//
+//  Created by Cons Bulaqueña on 19/03/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

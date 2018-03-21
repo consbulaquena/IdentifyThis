ae0ad373cb8e64c4e141a178b0a9858b88568268
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-21 15:29:44 +0800

    import AVKit /Vision APIs

--- IdentifyThis/ViewController.swift
+++ IdentifyThis/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import AVKit
+import Vision
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet var objectLabel: UILabel!
+    @IBOutlet var identifyButtonOutlet: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func Identify_TouchUpInside(_ sender: Any) {
     }
-
-
+    
 }
 

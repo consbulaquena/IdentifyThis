63674d5c8f58ff5b79ae1d275804de46a50f75a9
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-21 17:47:37 +0800

    Working UI button

--- IdentifyThis/ViewController.swift
+++ IdentifyThis/ViewController.swift
@@ -11,20 +11,48 @@ import AVKit
 import Vision
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    //AVcapture -everytime camera gets, record frame, tell delegate or view the frame
+    
     @IBOutlet var objectLabel: UILabel!
     @IBOutlet var identifyButtonOutlet: UIButton!
+    
+    var captureSession: AVCaptureSession!
+    var identifying = false //stop button
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    captureSession = AVCaptureSession()
+    
+        
     }
 
 
     @IBAction func Identify_TouchUpInside(_ sender: Any) {
-    }
-    
+        
+        //switch statement
+        identifying = !identifying
+        
+        if identifying {
+           
+            //if you set the image on another  UIButton
+
+            
+            identifyButtonOutlet.setImage(UIImage(named: "stop-1"), for: .normal)
+        } else {
+            identifyButtonOutlet.setImage(UIImage(named: "identify-160"), for: [])
+            
+
+
+        }
+        
+        }
+        
 }
+    
+
 
